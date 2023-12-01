57966ad24615ff2fac4ff34568c5d31271dcd082
zhangkaiyue <zhangkaiyue>
2023-12-02 04:30:26 +0800

    nio

--- src/main/java/com/zky/server/ServerProxy.java
+++ src/main/java/com/zky/server/ServerProxy.java
@@ -9,12 +9,15 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ServerProxy {
-    private Map<String,SocketChannel> clientSocketChannelMap = new ConcurrentHashMap<>();
+    private Map<String, SocketChannel> clientSocketChannelMap = new ConcurrentHashMap<>();
+    private String clientHost = "yunzky";
+    private Integer clientPort = 3306;
 
     public static void main(String[] args) throws Exception {
         new ServerProxy().run();
     }
-    public void run() throws Exception{
+
+    public void run() throws Exception {
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
         serverSocketChannel.configureBlocking(false);
         serverSocketChannel.bind(new InetSocketAddress(9999));
@@ -33,58 +36,104 @@ public class ServerProxy {
             while (iterator.hasNext()) {
                 SelectionKey sk = iterator.next();
                 if (sk.isAcceptable()) {
+                    System.out.println("执行接受方法");
                     SocketChannel socketChannel = serverSocketChannel.accept();
                     socketChannel.configureBlocking(false);
-                    socketChannel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE,UUID.randomUUID().toString());
+                    String uuid = UUID.randomUUID().toString();
+                    socketChannel.register(selector, SelectionKey.OP_READ, uuid);
 
-                } else if (sk.isReadable()) {
-                    SocketChannel clientSocketChannel = SocketChannel.open(new InetSocketAddress("localhost", 8080));
-                    clientSocketChannelMap.put(sk.attachment().toString(),clientSocketChannel);
+                    if (!clientSocketChannelMap.containsKey(uuid)) {
+                        SocketChannel clientSocketChannel = SocketChannel.open(new InetSocketAddress(clientHost, clientPort));
+                        clientSocketChannel.configureBlocking(false);
+                        clientSocketChannelMap.put(uuid, clientSocketChannel);
+                    }
+                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                    int num = 0;
+                    boolean flag = false;
+                    while (num <= 5){
+                        int read = clientSocketChannelMap.get(uuid).read(byteBuffer);
+                        if(read == -1){
+                            break;
+                        }else if(read == 0) {
+                            if(flag){
+                                num++;
+                            }
+                        }else {
+                            flag = true;
+                            byteBuffer.flip();
+                            socketChannel.write(byteBuffer);
+                            System.out.println(new String(byteBuffer.array()));
+                            byteBuffer.clear();
+                            num = 0;
+                        }
+                    }
+
+
+                }else if (sk.isReadable()) {
+                    System.out.println("执行读方法");
+                    if (!clientSocketChannelMap.containsKey(sk.attachment().toString())) {
+                        SocketChannel clientSocketChannel = SocketChannel.open(new InetSocketAddress(clientHost, clientPort));
+                        clientSocketChannel.configureBlocking(false);
+                        clientSocketChannelMap.put(sk.attachment().toString(), clientSocketChannel);
+                    }
                     SocketChannel socketChannel = (SocketChannel) sk.channel();
                     ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
                     int num = 0;
-                    while (num <= 10) {
+                    boolean flag = false;
+                    while (num <= 5) {
                         int read = socketChannel.read(byteBuffer);
                         if (read == -1) {
                             break;
                         } else if (read == 0) {
-                            num++;
+                            if(flag){
+                                num++;
+                            }
                         } else {
+                            flag = true;
                             byteBuffer.flip();
-                            clientSocketChannel.write(byteBuffer);
+                            clientSocketChannelMap.get(sk.attachment().toString()).write(byteBuffer);
                             System.out.println(new String(byteBuffer.array()));
                             byteBuffer.clear();
                             num = 0;
                         }
                     }
                     sk.interestOps(SelectionKey.OP_WRITE);
-                } else if (sk.isWritable()) {
-                    if(clientSocketChannelMap.containsKey(sk.attachment())){
-                        SocketChannel socketChannel = (SocketChannel) sk.channel();
-                        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-                        int num = 0;
-                        while (num <= 10) {
-                            try {
-                                int read = clientSocketChannelMap.get(sk.attachment().toString()).read(byteBuffer);
-                                if (read == -1) {
-                                    break;
-                                }else if(read == 0){
-                                    num++;
-                                } else {
-                                    byteBuffer.flip();
-                                    socketChannel.write(byteBuffer);
-                                    System.out.println(new String(byteBuffer.array()));
-                                    byteBuffer.clear();
-                                    num = 0;
-                                }
-                            } catch (Exception e) {
-                                break;
-                            }
-                        }
-                        clientSocketChannelMap.get(sk.attachment().toString()).close();
-                        clientSocketChannelMap.remove(sk.attachment().toString());
-                        sk.cancel();
+                }else if (sk.isWritable()) {
+                    System.out.println("执行写方法");
+                    if (!clientSocketChannelMap.containsKey(sk.attachment().toString())) {
+                        SocketChannel clientSocketChannel = SocketChannel.open(new InetSocketAddress(clientHost, clientPort));
+                        clientSocketChannel.configureBlocking(false);
+                        clientSocketChannelMap.put(sk.attachment().toString(), clientSocketChannel);
                     }
+                    SocketChannel socketChannel = (SocketChannel) sk.channel();
+                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                    int num = 0;
+                    boolean flag = false;
+                    while (num <= 5) {
+                        try {
+                            int read = clientSocketChannelMap.get(sk.attachment().toString()).read(byteBuffer);
+                            if (read == -1) {
+                                break;
+                            } else if (read == 0) {
+                                if(flag){
+                                    num++;
+                                }
+                            } else {
+                                flag = true;
+                                byteBuffer.flip();
+                                socketChannel.write(byteBuffer);
+                                System.out.println(new String(byteBuffer.array()));
+                                byteBuffer.clear();
+                                num = 0;
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            break;
+                        }
+                    }
+                    clientSocketChannelMap.get(sk.attachment().toString()).close();
+                    clientSocketChannelMap.remove(sk.attachment().toString());
+                    sk.cancel();
                 }
                 iterator.remove();
             }
